11ee2838a468a95dddba50740eba4802ed835dbe
Kevin BTA <kevinleitesolar@hotmail.com>
2025-06-17 17:31:33 -0300

    Adiciona estrutura e estilo ao layout do sorteador, implementa lógica de sorteio e remove arquivos desnecessários
    Add structure and style of layout(almost finalized)
    
    - implement of logics in JS

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const inputContainer = document.querySelector(".input-container");
+const form = document.querySelector("form")
+const inputNumbers = document.getElementById("numbers")
+const inputFrom = document.getElementById("from")
+const inputTo = document.getElementById("to")
 
-inputContainer.addEventListener("mouseover", () => {
-  inputContainer.classList.toggle('active')
-});
-inputContainer.addEventListener('click', () => {
-  inputContainer.classList.add('active');
-})
+let drawnNumbers = []
+
+inputNumbers.oninput = () => {
+	let value = inputNumbers.value.replace(/\D+/g, "")
+	inputNumbers.value = value
+}
+
+form.onsubmit = (e) => {
+	e.preventDefault()
+
+	for (let i = 0; i < inputNumbers.value; i++) {
+		drawnNumbers.push(drawnNumber(inputFrom.value, inputTo.value))
+	}
+
+	form.reset()
+
+}
+
+function drawnNumber(from, to) {
+	let min = Math.ceil(from)
+	let max = Math.floor(to)
+
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function 
