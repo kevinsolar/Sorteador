1ced270ec20f8cd68ffe0c2339c2cc482692b236
Kevin BTA <kevinleitesolar@hotmail.com>
2025-06-18 17:26:59 -0300

    Make the result box, finalize the styling - initial
    now needs to make the drawn numbers to appear dynamicly

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const inputTo = document.getElementById("to")
 
 let drawnNumbers = []
 
+// Limpa o input de numeros que vem de input type text
 inputNumbers.oninput = () => {
 	let value = inputNumbers.value.replace(/\D+/g, "")
 	inputNumbers.value = value
 }
 
+// trata o envio do formulário
 form.onsubmit = (e) => {
 	e.preventDefault()
 
@@ -18,9 +20,12 @@ form.onsubmit = (e) => {
 	}
 
 	form.reset()
+	form.remove()
 
+	resultSection(drawnNumbers)
 }
 
+// sorteia os numeros
 function drawnNumber(from, to) {
 	let min = Math.ceil(from)
 	let max = Math.floor(to)
@@ -28,4 +33,32 @@ function drawnNumber(from, to) {
 	return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function 
+function resultSection(drawnNumbers) {
+	try {
+		const result = document.getElementById("result")
+
+		// title
+		const resultTitle = document.createElement("div")
+		resultTitle.classList.add("title")
+		resultTitle.innerHTML = "<h2>Resultado do sorteio</h2>"
+
+		// content numbers
+		const resultContentN = document.createElement("div")
+		resultContentN.classList.add("content-number")
+
+		// the number
+		const resultNumberWrapper = document.createElement("div")
+		resultNumberWrapper.classList.add("number flex all-center")
+
+    // background dos numeros
+    const numberBG = document.createElement("div")
+    numberBG.classList.add("bg")
+
+    // the number chosen
+    const theNumber = document.createElement("span")
+    theNumber.classList.add("number")
+
+	} catch (error) {
+		console.log(error)
+	}
+}
